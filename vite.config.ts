facecf04125ad04ccdf2c39bd923ac644af15f85
Edantuti <edantuti@gmail.com>
2023-10-14 00:58:29 +0530

    fix: code splitting for node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin()
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id: string) => {
+          if (id.includes('react-router-dom') || id.includes("@remix-run") || id.includes('react-router')) {
+            return '@react-router'
+          }
+
+          if (id.includes('swiper')) {
+            return '@swiper'
+          }
+          if (id.includes('axios')) {
+            return '@axios'
+          }
+          if (id.includes('@dicebear')) {
+            return '@dicebear'
+          }
+          if (id.includes('lodash')) {
+            return '@lodash'
+          }
+          if (id.includes("victory")) {
+            return '@victory'
+          }
+          if (id.includes("exiftool-vendored")) {
+            return '@exiftool-vendored'
+          }
+        }
+      }
+    }
+  }
 });
