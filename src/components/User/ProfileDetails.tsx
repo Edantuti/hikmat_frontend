7dd1f2d68ea677cc77f29407e518e9db30c19aae
Edantuti <edantuti@gmail.com>
2023-11-28 10:11:51 +0530

    Changed the description

--- src/components/User/ProfileDetails.tsx
+++ src/components/User/ProfileDetails.tsx
@@ -27,7 +27,7 @@ const ProfileDetails: FC = (): JSX.Element => {
       <section className="w-[90%] h-[75vh] space-y-2 border mt-2 rounded p-4">
         <h2 className="text-3xl">Profile Details</h2>
         <div className="space-y-2">
-          <img src={userData.profile_url} className="rounded-full h-24 w-24 border" />
+          <img src={userData.profile_url + "?type=low"} className="rounded-full h-24 w-24 border" />
           <div className="text-lg space-y-2">
             <p className="text-xl">First Name:{userData.first}</p>
             <p className="text-xl">Last Name:{userData.last}</p>
